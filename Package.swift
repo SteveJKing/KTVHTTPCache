b0f0a111f6de83d7fdf229fbb128d78c9764f0b7
Bond <wenjie4835@163.com>
2024-08-01 23:22:05 +0800

    SPM

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "KTVHTTPCache",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "KTVHTTPCache",
+            targets: ["KTVHTTPCache"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", from: "7.6.4"),
+    ],
+    targets: [
+        .target(
+            name: "KTVHTTPCache",
+            dependencies: [
+                .product(name: "CocoaAsyncSocket", package: "CocoaAsyncSocket")
+            ],
+            path: "KTVHTTPCache",
+            exclude: ["Info.plist"],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("Classes/KTVHCCommon"),
+                .headerSearchPath("Classes/KTVHCDataStorage")
+            ]
+        )
+    ]
+)
+
